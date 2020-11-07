8babdbbbc606998b67fa79b95269fb074325ebf9
Paul-Jambou <60396677+Paul-Jambou@users.noreply.github.com>
2020-11-07 23:34:58 +0100

    Add files via upload
    pas finis

---
+++ view/categorie/formCreate.php
@@ -0,0 +1,20 @@
+<form method="post" action="index.php?controller=categorie">
+    <fieldset>
+        <legend>Ajouter une Catégorie:</legend>
+        <p>
+            <?php $controller = static::$object; ?>
+
+            <input type='hidden' name='action' value='<?php echo $name ?>'>
+
+            <label for="prenomAuteur_id">prenom auteur</label> :
+            <input type="text"  name="prenomAuteur" id="prenomAuteur_id" required/>
+
+            <label for="nomAuteur_id">nom auteur</label> :
+            <input type="text"  name="nomAuteur" id="nomAuteur_id" required/>
+
+        </p>
+        <p>
+            <input type="submit" value="Envoyer" />
+        </p>
+    </fieldset>
+</form>
