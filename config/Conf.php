5d0c2f0501f25569fdc5d4e925d6512ed21415f2
matteo-michel <60384880+matteo-michel@users.noreply.github.com>
2021-01-27 09:05:30 +0100

    Update Conf.php

--- config/Conf.php
+++ config/Conf.php
@@ -4,10 +4,10 @@ class Conf {
   private static $debug = false;
 
   static private $databases = array(
-    'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
-    'database' => 'michelm',
-    'login' => 'michelm',
-    'password' => 'password'
+    'hostname' => '',
+    'database' => '',
+    'login' => '',
+    'password' => ''
   );
 
    
